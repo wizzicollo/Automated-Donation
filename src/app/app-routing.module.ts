16c01b657e0b816351e70a8f09ea0954ecfec75e
Ianadika44 <adika19ian@gmail.com>
2020-06-25 14:13:35 +0300

    landing page and nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'services',component:HomeComponent},
+  {path:'aboutus',component:HomeComponent},
+  {path:'donations',component:HomeComponent},
+  {path:'register',component:HomeComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
+    
+import { MatIconModule } from '@angular/material/icon';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { HomeFormComponent } from './home-form/home-form.component';
+import { FormsModule } from '@angular/forms';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+
+
+ 
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HomeFormComponent,
+    TestimonialsComponent
+   
+  
+   
+    
+   
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule
+    
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
